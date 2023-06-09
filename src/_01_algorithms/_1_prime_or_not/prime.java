a47330e6fae8c059e925299b53e0571143f6332c
league <league@imac-29.attlocal.net>
2023-06-09 16:59:02 -0700

    yjnt

---
+++ src/_01_algorithms/_1_prime_or_not/prime.java
@@ -0,0 +1,14 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class prime {
+
+	public static void main(String[] args) {
+		String prime = JOptionPane.showInputDialog("Input a number");
+		int primeAsInt = Integer.parseInt(prime);
+		if primeAsInt % 2
+
+	}
+
+}
